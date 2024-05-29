00d2b5d61610614377bd1f9ca2fb0f7e288a41b7
soyung sim <simsy1024@gmail.com>
2024-05-29 19:54:20 +0900

    City App

---
+++ CityApp/Data/extension.swift
@@ -0,0 +1,8 @@
+//
+//  extension.swift
+//  CityApp
+//
+//  Created by 심소영 on 5/29/24.
+//
+
+import UIKit
---
+++ CityApp/cell/CityTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CityTableViewCell.swift
+//  CityApp
+//
+//  Created by 심소영 on 5/29/24.
+//
+
+import UIKit
+
+class CityTableViewCell: UITableViewCell {
+
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
